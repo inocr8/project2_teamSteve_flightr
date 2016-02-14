04191794e3408b0225711d981017dc5b8e62d478
Sky Su <februaryskys@gmail.com>
2016-02-14 14:50:36 +0000

    Formats outbound and return dates

--- client/src/itinerary/itinerary.js
+++ client/src/itinerary/itinerary.js
@@ -7,9 +7,26 @@ var Itinerary = function(options){
     this.outboundDate = options.outboundDate;
     this.returnDate = options.returnDate;
 
+    this.dates = this.formatDates(this.outboundDate, this.returnDate);
+
     this.destination = options.arrivalAirport;
     this.checkin = null;
     this.checkout = null;
+};
+
+Itinerary.prototype = {
+    formatDates: function(outboundDate, returnDate){
+        var dateOptions = {
+            weekday: 'short',
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric'
+        };
+        return {
+            outboundDate: outboundDate.toLocaleDateString('en-GB', dateOptions),
+            returnDate: returnDate.toLocaleDateString('en-GB', dateOptions)
+        };
+    }
 };
 
 module.exports = Itinerary;
