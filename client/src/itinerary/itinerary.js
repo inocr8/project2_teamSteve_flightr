a43fa3e6f54fdffb74d8a75991a84bd94a20f778
Sky Su <februaryskys@gmail.com>
2016-02-15 11:35:48 +0000

    Caclulates number of nights

--- client/src/itinerary/itinerary.js
+++ client/src/itinerary/itinerary.js
@@ -7,26 +7,46 @@ var Itinerary = function(options){
     this.outboundDate = options.outboundDate;
     this.returnDate = options.returnDate;
 
-    this.displayDates = this.formatDisplayDates(this.outboundDate, this.returnDate);
+    this.displayDates = null
 
     this.destination = options.arrivalAirport;
     this.checkin = null;
     this.checkout = null;
+    this.numberOfNights = null;
 };
 
 Itinerary.prototype = {
-    formatDisplayDates: function(outboundDate, returnDate){
+    updateCheckinCheckoutDates: function(checkin, checkout){
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.formatDisplayDates();
+        this.updateNumberOfNights();
+    },
+
+    formatDisplayDates: function(){
         var dateOptions = {
             weekday: 'short',
             day: 'numeric',
             month: 'short',
             year: 'numeric'
         };
-        return {
-            outboundDate: outboundDate.toLocaleDateString('en-GB', dateOptions),
-            returnDate: returnDate.toLocaleDateString('en-GB', dateOptions)
+        this.displayDates =  {
+            outboundDate: this.outboundDate.toLocaleDateString('en-GB', dateOptions),
+            returnDate: this.returnDate.toLocaleDateString('en-GB', dateOptions),
+            checkin: this.checkin.toLocaleDateString('en-GB', dateOptions),
+            checkout: this.checkout.toLocaleDateString('en-GB', dateOptions)
         };
+    },
+
+    updateNumberOfNights: function(){
+        this.numberOfNights = this.calcNumberOfNights(this.checkin, this.checkout);
+    },
+
+    calcNumberOfNights: function(checkin, checkout){
+        var oneDay = 24 * 60 * 60 * 1000;
+        return Math.abs( (checkout.getTime() - checkin.getTime()) / oneDay );
     }
+
 };
 
 module.exports = Itinerary;
