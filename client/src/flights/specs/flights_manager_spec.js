9cb199df10f3821160aead20f742844167b6c2dd
Sky Su <februaryskys@gmail.com>
2016-02-15 09:11:08 +0000

    Fixes Model to Manager

--- client/src/flights/specs/flights_manager_spec.js
+++ client/src/flights/specs/flights_manager_spec.js
@@ -1,11 +1,11 @@
-var FlightsModel = require('../flightsModel.js');
+var FlightsManager = require('../flightsManager.js');
 var outgoingFlightsData = require('./outgoing_flights_test_data.json');
 var returnFlightsData = require('./return_flights_test_data.json');
 var expect = require('chai').expect;
 
 describe('Flights', function(){
     beforeEach(function createFlights(){
-        flights = new FlightsModel();
+        flights = new FlightsManager();
     });
 
     it('should have no data at start', function(){
@@ -35,7 +35,7 @@ describe('Flights', function(){
 
 describe('Flights', function(){
     beforeEach(function createFlightsAndSeedData(){
-        flights = new FlightsModel();
+        flights = new FlightsManager();
         flights.addFlights(outgoingFlightsData);
     });
 
@@ -97,7 +97,7 @@ describe('Flights', function(){
 
 describe('Flights', function(){
     beforeEach(function createFlightsAndSeedData(){
-        flights = new FlightsModel();
+        flights = new FlightsManager();
         flights.addFlights(outgoingFlightsData);
         flights.addFlights(returnFlightsData);
     });
