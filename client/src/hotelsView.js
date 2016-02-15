8283402bb53084e013e83484fea3b2f172baabd4
Sky Su <februaryskys@gmail.com>
2016-02-15 14:54:22 +0000

    Update hotelMap name and map on click

--- client/src/hotelsView.js
+++ client/src/hotelsView.js
@@ -3,7 +3,7 @@ var HotelMap = require('./map/map.js');
 
 var HotelsView = function(packageOptions){
     this.packageOptions = packageOptions;
-    this.map = new HotelMap(packageOptions.currentPackage.hotel);
+    this.hotelMap = new HotelMap(packageOptions.currentPackage.hotel);
     this.element = document.querySelector('#hotels');
 };
 
@@ -13,7 +13,7 @@ HotelsView.prototype = {
         var hotels = this.packageOptions.hotels;
         for (var key in hotels) {
             var hotel = hotels[key];
-            this.map.addMarker(hotel);
+            this.hotelMap.addMarker(hotel);
         }
     },
 
@@ -63,8 +63,9 @@ HotelsView.prototype = {
     notifyHotelSelection: function(element){
         var key = element.id;
 
-        this.packageOptions.updateCurrentPackageHotel(key);
+        var hotel = this.packageOptions.updateCurrentPackageHotel(key);
         this.rebuildHotelOptions(this.packageOptions);
+        this.hotelMap.setCenter(hotel);
 
         console.log('current', this.packageOptions.currentPackage);
     }
