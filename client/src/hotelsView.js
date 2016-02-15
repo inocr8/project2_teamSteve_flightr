7d4403c0d45b210fb5132f00d0201e96c6aaeae6
Sky Su <februaryskys@gmail.com>
2016-02-15 10:38:28 +0000

    Initializes hotels view in view, and renders

--- client/src/hotelsView.js
+++ client/src/hotelsView.js
@@ -1,3 +1,5 @@
+var Mustache = require('mustache');
+
 var HotelsView = function(packageOptions){
     this.packageOptions = packageOptions;
     // this.map = map;
@@ -6,9 +8,44 @@ var HotelsView = function(packageOptions){
 
 HotelsView.prototype = {
 
-    rebuildHotelsOptions: function(){
+    rebuildHotelOptions: function(){
+        console.log('hotels view called');
         this.element.innerHTML = Mustache.render('<p>{{itinerarary.destination}} Hotels</p>', this.packageOptions);
-        
-    }
+
+        var hotels = this.packageOptions.hotels;
+        for (var key in hotels) {
+
+            var hotel = hotels[key];
+
+            var li = document.createElement('li');
+            li.classList.add('hotel-options');
+
+            var a = document.createElement('a');
+            a.id = key;
+
+            var view = {
+                hotel: hotel,
+                stars: hotel.stars > 1 ? 'stars' : 'star'
+            }
+
+            a.innerHTML = Mustache.render(
+                        '<span class="price">£{{hotel.pricePerPerson}}pp</span>'
+            +           '<span class="name">{{hotel.name}}</span>'
+            +           '<span class="stars">{{hotel.stars}} {{stars}}</span>', view);
+
+            if (hotel === this.packageOptions.bestValuePackage.hotel) {
+                li.classList.add('best-value');
+            }
+
+            if (hotel === this.packageOptions.currentPackage.hotel) {
+                li.classList.add('selected');
+            }
+
+            li.appendChild(a);
+            this.element.appendChild(li);
+        }
+    },
 
 };
+
+module.exports = HotelsView;
