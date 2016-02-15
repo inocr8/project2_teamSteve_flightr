c12a4a2499f41fd6535e20a4934530db0bf4c258
Euan Walls <apps@inocr8.co>
2016-02-15 16:44:36 +0000

    added lodash, refactored manager file and added delete option

--- client/src/localStorage/localStorageManager.js
+++ client/src/localStorage/localStorageManager.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var LocalStorageManager = function(){
 
     this.storageKeyPackages ="flightrPackages";
@@ -16,8 +18,25 @@ LocalStorageManager.prototype = {
   },
 
   savePackage:function(package){
-    this.packages.push(package);
-    localStorage.setItem(this.storageKeyPackages, JSON.stringify(this.packages));
+    var storedPackages = this.packages;
+    for ( var i = 0; i < storedPackages.length; i++ ) {
+      if ( _.isEqual(storedPackages[i], package ) ){
+        console.log('Already saved');
+        return;
+      }
+    }
+    storedPackages.push(package);
+    localStorage.setItem(this.storageKeyPackages, JSON.stringify(storedPackages));
+  },
+
+  deletePackage:function(package){
+    var storedPackages = this.packages;
+    for ( var i = 0; i < storedPackages.length; i++ ) {
+      if ( _.isEqual(storedPackages[i], package ) ){
+        storedPackages.splice(i,1); 
+      }
+    }
+    localStorage.setItem(this.storageKeyPackages, JSON.stringify(storedPackages));
   },
 
   getItineraries:function(){
@@ -27,8 +46,25 @@ LocalStorageManager.prototype = {
   },
 
   saveItinerary:function(itinerary){
-    this.itineraries.push(itinerary);
-    localStorage.setItem(this.storageKeyItineraries, JSON.stringify(this.itineraries));
+    var storedItineraries = this.itineraries;
+    for ( var i = 0; i < storedItineraries.length; i++ ) {
+      if ( _.isEqual(storedItineraries[i], itinerary ) ){
+        console.log('Already saved');
+        return;
+      }
+    }
+    storedItineraries.push(itinerary);
+    localStorage.setItem(this.storageKeyItineraries, JSON.stringify(storedItineraries));
+  },
+
+  deleteItinerary:function(itinerary){
+    var storedItineraries = this.itineraries;
+    for ( var i = 0; i < storedItineraries.length; i++ ) {
+      if ( _.isEqual(storedItineraries[i], itinerary ) ){
+        storedItineraries.splice(i,1); 
+      }
+    }
+    localStorage.setItem(this.storageKeyItineraries, JSON.stringify(storedItineraries));
   },
 };
 
