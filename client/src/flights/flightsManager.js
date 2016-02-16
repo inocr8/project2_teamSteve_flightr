82498620ea3cc98bf61ee291e4b2211c484b148a
Sky Su <februaryskys@gmail.com>
2016-02-16 22:09:29 +0000

    Tidies up FlightManager

--- client/src/flights/flightsManager.js
+++ client/src/flights/flightsManager.js
@@ -3,8 +3,7 @@ var moment = require('moment');
 var Flight = require('./flight.js');
 
 var FlightsManager = function(){
-    var self = this;
-    self.data = [];
+    this.data = [];
 };
 
 FlightsManager.prototype = {
@@ -84,8 +83,8 @@ FlightsManager.prototype = {
         var returnFlights = this.flightQuery(returnRequest);
 
         return {
-            outboundFlights: this.sortByPrice(outboundFlights),
-            returnFlights: this.sortByPrice(returnFlights)
+            outboundFlights: this.sortByDeparting(outboundFlights),
+            returnFlights: this.sortByDeparting(returnFlights)
         };
     },
 
@@ -138,13 +137,7 @@ FlightsManager.prototype = {
         for (flight of flightsArray) {
             this.addFlight(flight);
         }
-    },
-
-    // sameDay: function(date1, date2){
-    //     return date1.getDate() === date2.getDate()
-    //         && date1.getMonth() === date2.getMonth()
-    //         && date1.getFullYear() === date2.getFullYear();
-    // }
+    }
 };
 
 
