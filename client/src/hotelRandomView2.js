43eb6f2c198803a06f5f4f453c328b3089d8fd2f
Euan Walls <apps@inocr8.co>
2016-02-18 00:28:36 +0000

    slight refactoring on the randomizer box css

--- client/src/hotelRandomView2.js
+++ client/src/hotelRandomView2.js
@@ -9,7 +9,7 @@ var HotelRandomView = function(hotelsManager){
 HotelRandomView.prototype = {
   buildRandomHotel: function(){
     var output = Mustache.render(
-        '<div class="hotel-info">'+'<h2>'+'Hotel Name: {{name}}'+'</h2>'+'<h4>'+'Stars: {{stars}}, Price Per Person: £{{pricePerPerson}}, Address: {{address.building}} {{address.street}} {{address.city}},{{address.zip}}'+'</h4>'+'</div><img src="{{address.image}}" style="float:right"/>'
+        '<div class="hotel-info">'+'<h2>'+'{{name}}'+'</h2>'+'<h3>'+'Stars: {{stars}}'+'</h3>'+'<h4>'+'Price Per Person: £{{pricePerPerson}}'+'<br>'+'Address: {{address.building}} {{address.street}} {{address.city}},{{address.zip}}'+'</h4>'+'</div><img src="{{address.image}}" style="float:right"/>'
         , this.randomHotel);
     this.element.innerHTML = output;
   }
