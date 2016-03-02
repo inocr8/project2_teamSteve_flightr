fbb894393c45c2151f5bad16bbac2c94f41b7f12
Sky Su <februaryskys@gmail.com>
2016-03-02 22:42:12 +0000

    Refactors tests to use moment.js

--- client/src/itinerary/specs/itinerary_spec.js
+++ client/src/itinerary/specs/itinerary_spec.js
@@ -1,13 +1,22 @@
 var Itinerary = require('../itinerary.js');
 var expect = require('chai').expect;
+var moment = require('moment');
 
 describe('Itinerary', function(){
 
+    it('should update checkin and checkout dates', function() {
+        var checkin = moment('2016-03-29');
+        var checkout = moment('2016-04-11');
+        Itinerary.prototype.updateCheckinCheckoutDates(checkin, checkout);
+        expect(Itinerary.prototype.checkin).to.equal(checkin);
+        expect(Itinerary.prototype.checkout).to.equal(checkout);
+    });
+
     it('should calculate the number of nights given checkin and checkout', function(){
-        var checkin = new Date('2016-03-29');
-        console.log(checkin);
-        var checkout = new Date('2016-04-11');
-        var nights = Itinerary.prototype.calcNumberOfNights(checkin, checkout);
+        Itinerary.prototype.checkin = moment('2016-03-29');
+        Itinerary.prototype.checkout = moment('2016-04-11');
+        var nights = Itinerary.prototype.numberOfNights();
         expect(nights).to.equal(13);
     });
+
 });
